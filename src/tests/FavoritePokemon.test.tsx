74a698759c167e110f1681f2dfef32250a90dfd1
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2024-02-20 03:56:37 -0300

    Req 03 - resolução

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,31 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <FavoritePokemon.tsx />', () => {
+  it('1. A mensagem "No favorite pokemon found" deve ser exibida na tela a mensagem caso a pessoa não tenha Pokémon favorito', () => {
+    renderWithRouter(<App />, { route: '/favorites' });
+
+    const notFoundMsg = screen.getByText(/no favorite pokémon found/i);
+    expect(notFoundMsg).toBeInTheDocument();
+  });
+
+  it('2. Ao favoritar um pokemón através de "ver detalhes", ele deve ser exibido na página de Favoritos', async () => {
+    const { user } = renderWithRouter(<App />);
+    const pokemonName = screen.getByText(/pikachu/i);
+    expect(pokemonName).toBeInTheDocument();
+
+    const linkDetails = screen.getByRole('link', { name: /more details/i });
+    await user.click(linkDetails);
+
+    const checkboxFavorite = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i }) as HTMLInputElement;
+    expect(checkboxFavorite.checked).toBe(false);
+    await user.click(checkboxFavorite);
+    expect(checkboxFavorite.checked).toBe(true);
+
+    const linkFavorite = screen.getByRole('link', { name: /favorite pokémon/i });
+    await user.click(linkFavorite);
+    const favPokemonName = screen.getByText(/pikachu/i);
+    expect(favPokemonName).toBeInTheDocument();
+  });
+});
