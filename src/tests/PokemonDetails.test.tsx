81f2c3e364159afa89cd0a245cb1d7fab064fd08
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2024-02-20 22:47:22 -0300

    retirada dos skips em FavoritePokemon.test

--- src/tests/PokemonDetails.test.tsx
+++ src/tests/PokemonDetails.test.tsx
@@ -1,1 +1,25 @@
-test('', () => {});
+describe('Teste o componente <PokemonDetails.tsx />', () => {
+  it.skip('1. A página deve conter um texto <name> Details, em que <name> é o nome do Pokémon.', () => {
+  });
+
+  it.skip('2. Não deve existir o link de navegação para os detalhes do Pokémon selecionado', () => {
+  });
+
+  it.skip('3. A seção de detalhes deve conter um heading h2 com o texto Summary.', () => {
+  });
+
+  it.skip('4. A seção de detalhes deve conter um parágrafo com o resumo do Pokémon específico sendo visualizado', () => {
+  });
+
+  it.skip('', () => {
+  });
+
+  it.skip('', () => {
+  });
+
+  it.skip('', () => {
+  });
+
+  it.skip('', () => {
+  });
+});
